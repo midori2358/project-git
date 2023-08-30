d85a21c1fcefc91e9abccd4b5fd0b70d886482be
midori.shinozaki <bltrefutation@gmail.com>
2023-08-30 01:45:41 +0000

    user page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,10 @@
     <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
         <div class="hero-content text-center my-10">
             <div class="max-w-md mb-10">
-                <h2>Welcome to the Microposts</h2>
+                 <h2>やりたいこと、みんなでやろう</h2>
+                 <p>ひとりでは難しい、でも一人じゃなかったら？</p>
+                 <p>どうすればいいかわからない、でも皆のアイデアがあったら？</p>
+                 <p>あなたの夢、だれかの挑戦、やってみよう</p>
                 {{-- ユーザ登録ページへのリンク --}}
                 <a class="btn btn-primary btn-lg normal-case" href="{{ route('register') }}">Sign up now!</a>
             </div>
